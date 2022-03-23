d4560bdfcae19a82b5125b2045df45e88d16eb4a
aanil3 <Abinav_anil@hotmail.com>
2022-03-23 00:27:21 -0400

    Sample Tasks + Button

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {TASKS} from '../../mock-tasks';
+import {task} from '../../task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: task[] = TASKS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
